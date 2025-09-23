46efc2f3e90ccf81016d48f2ebebe38791b4e0a6
Morné Maritz <morne.maritz@entelect.co.za>
2025-09-23 16:54:13 +0200

    lint configs. Docker and k8s

--- health-diary-ui/eslint.config.js
+++ health-diary-ui/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -11,13 +13,24 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      // strict rules
+      tseslint.configs.strictTypeChecked,
+      // stylistic rules
+      tseslint.configs.stylisticTypeChecked,      
+      // Enable lint rules for React
+      reactX.configs['recommended-typescript'],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: [ './tsconfig.json', './tsconfig.node.json', './tsconfig.app.json' ],
+        tsConfigRootDir: import.meta.dirname,
+      }
     },
   },
 ])
